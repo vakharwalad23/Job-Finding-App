17e8c353fd93f23dff7493755bcc534091894960
vakharwalad23 <105845275+vakharwalad23@users.noreply.github.com>
2023-03-19 20:51:21 +0530

    Initial

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+
+import styles from './welcome.style'
+
+const Welcome = () => {
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  )
+}
+
+export default Welcome
